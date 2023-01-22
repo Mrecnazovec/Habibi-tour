4dd290f3f18c4e455433a7636a0340caadf3b3fd
Mrecnazovec <101330874+Mrecnazovec@users.noreply.github.com>
2023-01-22 20:32:33 +0500

    Add files via upload

---
+++ uz/js/left.js
@@ -0,0 +1,11 @@
+let container = document.querySelector(".container");
+let pos_blue_box = document.querySelector(".pos_blue_box");
+
+let margin = window.getComputedStyle(container).marginLeft;
+console.log(margin);
+// margin = margin.slice(0, 3);
+margin = margin.replace('px','');
+margin = Number(margin) + 12;
+console.log(margin);
+
+pos_blue_box.style.left = margin + "px";
